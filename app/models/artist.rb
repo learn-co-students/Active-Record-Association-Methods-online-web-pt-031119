efcc20b0b89bba740ccaa0e0be447e563a037f75
David Stewart <davidstewart@gmail.com>
2019-06-08 14:14:42 -0600

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,8 +1,12 @@
+require 'pry'
+
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
+    binding.pry
+    
     #return the genre of the artist's first saved song
   end
 
@@ -13,4 +17,7 @@ class Artist < ActiveRecord::Base
   def genre_count
     #return the number of genres associated with the artist
   end
+
+
+
 end
